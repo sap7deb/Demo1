44a005fcdba9f707a80e9dcb0c5a242a059a7392
sap7deb <sap7deb@gmail.com>
2018-06-06 23:18:31 +0530

    Class added

---
+++ src/test/java/MavenDemotest/Mavwndemo/ClassTest1.java
@@ -0,0 +1,15 @@
+package MavenDemotest.Mavwndemo;
+
+import org.testng.annotations.Test;
+
+import junit.framework.TestCase;
+
+public class ClassTest1 extends TestCase {
+
+	
+		@Test
+	    public void testApp2()
+	    {
+	        assertTrue( true );
+	    }
+}
