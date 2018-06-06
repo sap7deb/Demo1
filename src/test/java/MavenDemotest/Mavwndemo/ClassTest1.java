28bf93fa43f987dab5b2f776f47362c9c2108db9
sap7deb <sap7deb@gmail.com>
2018-06-06 23:33:12 +0530

    Class changed

--- src/test/java/MavenDemotest/Mavwndemo/ClassTest1.java
+++ src/test/java/MavenDemotest/Mavwndemo/ClassTest1.java
@@ -6,7 +6,10 @@ import junit.framework.TestCase;
 
 public class ClassTest1 extends TestCase {
 
-	
+	public ClassTest1(String testName) {
+		// TODO Auto-generated constructor stub
+		super( testName );
+	}
 		@Test
 	    public void testApp2()
 	    {
